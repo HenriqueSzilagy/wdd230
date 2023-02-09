1a7264d67b16e0b42889c9b197f0bb1644b2a76a
HenriqueSzilagy <henriqueszilagy@gmail.com>
2023-02-09 17:56:19 -0300

    update site

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,7 @@
+const temperature = "23°C"
+const windSpeed = "Wind Speed: 24 km/h"
+const windChill = 'Wind Chill: N/A'
+
+document.getElementById('temperature').innerHTML = temperature
+document.getElementById('windSpeed').innerHTML = windSpeed
+document.getElementById('windChill').innerHTML = windChill
