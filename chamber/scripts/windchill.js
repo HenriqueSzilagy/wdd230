f74a9fa7d153191e7033a441ade240e4d2518f6a
HenriqueSzilagy <henriqueszilagy@gmail.com>
2023-02-17 02:04:55 -0300

    fixed

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,7 +1,20 @@
-const temperature = "23°C"
-const windSpeed = "Wind Speed: 24 km/h"
-const windChill = 'Wind Chill: N/A'
+const fahrenheit = 73.4
+const windSpeed = 24
 
-document.getElementById('temperature').innerHTML = temperature
-document.getElementById('windSpeed').innerHTML = windSpeed
-document.getElementById('windChill').innerHTML = windChill
+const celsius = Math.round((fahrenheit - 32) / 1.8);
+
+const windChillF = Math.round(35.74 + 0.6215 * fahrenheit - 35.75 * windSpeed ** 0.16 + 0.4275 * fahrenheit * windSpeed ** 0.16);
+
+
+const windChill = Math.round((windChillF - 32) / 1.8);
+
+document.getElementById('temperature').innerHTML = `${celsius}°C`;
+document.getElementById('windSpeed').innerHTML = `Wind Speed: ${windSpeed} km/h`;
+
+if (celsius <= 10 && windSpeed >= 4.8 ){
+    
+    document.getElementById('windChill').innerHTML = `Wind Chill: ${windChill}°C`;
+} else {
+    document.getElementById('windChill').innerHTML = "Wind Chill: N/A";
+}
+ 
