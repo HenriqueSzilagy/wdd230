98ea13a945bdcfd4d69af3b6e0b0db8a1ae00e44
HenriqueSzilagy <henriqueszilagy@gmail.com>
2023-02-25 09:28:46 -0300

    fix

--- chamber/scripts/localStorage.js
+++ chamber/scripts/localStorage.js
@@ -1,5 +1,3 @@
-
-
 const currentDate = new Date();
 
 
@@ -9,8 +7,11 @@ const lastVisited = localStorage.getItem('lastVisited');
 if (lastVisited) {
   const differenceInTime = currentDate.getTime() - Date.parse(lastVisited);
   const differenceInDays = Math.round(differenceInTime / (1000 * 3600 * 24));
-  document.getElementById('last-visited').textContent = differenceInDays;
+  if(differenceInDays === 0){
+    document.getElementById('last-visited').textContent = "You have never visited this page before"; 
+  } else {
+  
+  document.getElementById('last-visited').textContent = `You last visited this page ${differenceInDays} days ago`;
 }
-
-
+}
 localStorage.setItem('lastVisited', currentDate.toISOString());
